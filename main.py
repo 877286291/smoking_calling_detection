bbc99fbec0207475f93e82f9677e3da492e32d34
Aurora <877286291@qq.com>
2023-04-15 19:58:10 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,37 @@
+import cv2
+from ultralytics import YOLO
+
+# load a pretrained model (recommended for training)
+model = YOLO("yolov8n.pt")
+
+# 打开视频文件
+cap = cv2.VideoCapture(0)
+
+# 设置视频宽度和高度
+width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+while cap.isOpened():
+    # 读取视频的每一帧
+    ret, frame = cap.read()
+
+    if ret:
+        results = model.predict(source=frame, save=True)
+        for result in results:
+            for box in result.boxes:
+                x1, y1, x2, y2 = map(int, box.xyxy[0])
+                cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
+                cv2.putText(frame, f"{result.names[int(box.cls.data)]}:{box.conf.item():.2f}", (x1, y1),
+                            cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2,
+                            cv2.LINE_AA)
+        # 显示绘制后的图像
+        cv2.imshow('frame', frame)
+
+        # 设置时间间隔
+        if cv2.waitKey(25) & 0xFF == ord('q'):
+            break
+    else:
+        break
+
+# 释放摄像头并销毁窗口
+cap.release()
